731e2a6e508db8214e123238c2d70f9357e28365
divy soni <chetanshudha267@gmail.com>
2024-06-14 16:41:04 +0530

    word game done

--- string/task5.cpp
+++ string/task5.cpp
@@ -12,17 +12,42 @@ using namespace std;
 int main() {
     string hidden_word = "rainbow";
     string user_guess = "";
+    int count=0;
+
+    system("cls");
+    cout << "\t\t<------ Welcome to Word guess game ------>" << endl << endl
+         << " > You have 3 chance to guess the word after 3 guess you will get hint of the word ! Best of luck\n" << endl;
+    system("pause");
 
     while (user_guess.compare(hidden_word)) {
-        cout << "Enter a string: ";
+        system("cls");
+        cout << "\nEnter a Word: ";
         getline(cin, user_guess);
 
         if(hidden_word.compare(user_guess) == 0) {
-            cout << "Correct guess!" << endl;
-            break;
+            cout << "Correct guess! You won the game in " << count << " guesses !! congratulations..." << endl;
+            return 0;
         } else {
             system("cls");  //clear screen
+            count++;
             cout << "Try Again!" << endl;
+            if(count < 3){
+                cout << "You have " << (3-count) << " tries left!" << endl;
+                system("pause");
+            }
+            else {
+                if((count-3)==hidden_word.size()){ 
+                    cout << "Word was \"" << hidden_word << "\" , Best of luck next time :) " << endl;
+                    return 0;
+                }else {
+                    cout << "---> Hint : ";
+                    for(int i=0;i<count-2;i++){
+                        cout << hidden_word[i] ;
+                    }
+                    cout << endl;
+                    system("pause");
+                }
+            }
         }
     }
     
