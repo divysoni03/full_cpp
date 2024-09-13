cc225cbbb3e793067297bf4fb012274a85829e8a
divy soni <chetanshudha267@gmail.com>
2024-09-13 22:47:14 +0530

    string array and char array diff

---
+++ string/stringArray&charArray.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    string str[5] = "ABC";
+    cout << str[0] << endl;
+    cout << str[1] << endl; 
+    cout << str[2] << endl; 
+    cout << str[3] << endl;
+    cout << str[4] << endl;
+    // cout << str[5] << endl;
+    cout << str;
+    return 0;
+
+    char str[5] = "abc";
+    cout << str[3] << endl << str;
+}
